4e7db0de904897a054d6162e5eea49a4796e9cc3
franciscompinto <franciscomenezes94@ua.pt>
2015-03-27 23:11:15 +0000

    =>no programas Desafio2.c e um codigo na base no ano passado mas com as alteracoes aconselhados pelo jla
    => o txt sensor e a exprecao que da a distancia lida pelo sensor

--- Drunk_mouse/desafio2.c
+++ Drunk_mouse/desafio2.c
@@ -36,7 +36,6 @@ void Chegada_Farol (void);
 void Ver_Farol(void);
 void ANDAR1(void);
 void ANDAR2(void);
-void Sensor(void);
 void Fim(void);  
 void TimeOut(void);
 /********************************************/
@@ -47,49 +46,30 @@ int main (void)
   	closedLoopControl( true );
   	setVel2(0, 0);
 
-  	T4CONbits.TCKPS=5;
-PR4=(PBCLK/32/3)-1;//fout 20MHz/(32*(62499+1))*10Hz,freq a 1 hz
-TMR4=0;//Reset timer T2
-T4CONbits.TON=1;
-//Interrupts
-IFS0bits.T4IF=0;//reset do timer
-IPC4bits.T4IP=2;//priority
-IEC0bits.T4IE=1;//enable timer3
-EnableInterrupts();
 
 	printStr(__FILE__); // para saber o nome do ficheiro que esta a correr no robot
 	printf("\r    battery: %d ", batteryVoltage());
+	if(batteryVoltage() <94){
+		printf("_Bateria fraca, MUDAR Bateria\n");
+
+	}
 	printStr("\n");
 	
 
 //desligar os leds para o inicio
-	leds(0b1111);	//leds off
+	leds(0x0);	//leds off
 
 
-  #if 0
 
 	while(1)
 	{
 		while(!tick40ms);
 		tick40ms=0;
+		disableObstSens();
+		enableObstSens();
 		readAnalogSensors();
-		
 
 
-	}
-
-
-  #endif
-
-
-
-  while (TRUE)
-    {
-	
-	
-	TimeOut();		// timeOut => tb devia ir para uma inturrupcao
-
-	//Sensor(); 	 //leitura os sensores tem de se por numa interropecao
 
 	
 
@@ -99,23 +79,25 @@ EnableInterrupts();
 	if(startButton() == 1) // Botao start(preto) primido
 	{
 		estado = 1;
-		resetCoreTimer();
-		leds(0x1);
 	
 	}
 	else if(stopButton() == 1) //Botao stop(vermelho) primido
 	{
 		estado = 0;
-		leds(0x0);
 	}
 
  	//funcoa de funcionameneto
 	if(estado == 1) // robot fica em presesamento
 	{
+		
+		TimeOut();		// timeOut => tb devia ir para uma inturrupcao
+		Chegada_Farol();
 		Ver_Farol();
-		printf("D: %d   F: %d   E: %d     LINHA: %d    Farol: %d \r",	sensor_dir,sensor_frente,sensor_esq,linha ,farolsen );
 		ANDAR2();
+
+	
 	}
+
 	//quando e primido o botao stop
 	else if(stopButton() == 1)		// deslica o funcionamento, nenhum led activo
 	{
@@ -124,7 +106,11 @@ EnableInterrupts();
 	
 	}
 
-    }
+ 	
+ 	}
+
+
+
   return (0);
 }
 
@@ -175,45 +161,51 @@ void Rodar_Sobre_Si ()
 //########################################################################################################################################################################
 void ANDAR1 ()
 {
-		if(sensor_dir < LIMIAR && sensor_frente < LIMIAR && sensor_esq < LIMIAR )
+		if(obstacleSensor(OBST_SENSOR_RIGHT) < LIMIAR && obstacleSensor(OBST_SENSOR_FRONT) < LIMIAR && obstacleSensor(OBST_SENSOR_LEFT) < LIMIAR )
 		{	
 
 			andar_frente();
 		}
-		 if(sensor_dir > LIMIAR && sensor_frente > LIMIAR && sensor_esq > LIMIAR )
+		 if(obstacleSensor(OBST_SENSOR_RIGHT)> LIMIAR && obstacleSensor(OBST_SENSOR_FRONT) > LIMIAR && obstacleSensor(OBST_SENSOR_LEFT) > LIMIAR )
 		{
 			Rodar_Sobre_Si();
 		}
-		 if(sensor_dir < LIMIAR && sensor_frente > LIMIAR && sensor_esq > LIMIAR )
+		 if(obstacleSensor(OBST_SENSOR_RIGHT) < LIMIAR && obstacleSensor(OBST_SENSOR_FRONT) > LIMIAR && obstacleSensor(OBST_SENSOR_LEFT) > LIMIAR )
 		{
 			Vira_esq();
-			//Ajusta_Esq();
+		
 		}
-		 if(sensor_dir > LIMIAR && sensor_frente > LIMIAR && sensor_esq < LIMIAR )
+		 if(obstacleSensor(OBST_SENSOR_RIGHT) > LIMIAR && obstacleSensor(OBST_SENSOR_FRONT) > LIMIAR && obstacleSensor(OBST_SENSOR_LEFT) < LIMIAR )
 		{
 			Vira_Dir();
-			//Ajusta_Dir();
+		
 		}
 
 }
 //################3
 void ANDAR2 ()
 {
-	if(sensor_dir < LIMIAR)
+	if(obstacleSensor(OBST_SENSOR_FRONT) < LIMIAR)
+	{
+		setVel2(0,0);
+		Rodar_Sobre_Si();
+		wait(1);
+	}
+	else if(obstacleSensor(OBST_SENSOR_RIGHT) < LIMIAR)
 	{
 		Vira_Dir();
 		
 	}
-	if(sensor_esq > LIMIAR)
+	else if(obstacleSensor(OBST_SENSOR_LEFT) > LIMIAR)
 	{
 		Vira_esq();
 		
 	}
-	if(sensor_frente < LIMIAR && sensor_dir < LIMIAR&& sensor_esq < LIMIAR)
+	else if(obstacleSensor(OBST_SENSOR_FRONT) < LIMIAR && obstacleSensor(OBST_SENSOR_RIGHT) < LIMIAR && obstacleSensor(OBST_SENSOR_LEFT)< LIMIAR)
 	{	
 		andar_frente();
 	}
-	if(sensor_frente > LIMIAR && sensor_dir > LIMIAR && sensor_esq > LIMIAR)
+	else if(obstacleSensor(OBST_SENSOR_FRONT) > LIMIAR && obstacleSensor(OBST_SENSOR_RIGHT) > LIMIAR && obstacleSensor(OBST_SENSOR_LEFT)> LIMIAR)
 	{	
 		setVel2(0,0);
 		Rodar_Sobre_Si();
@@ -225,67 +217,6 @@ void ANDAR2 ()
 
 }
 
-//##################################################################################################################################################################3
-void Sensor()
-{
-	//int sum_dir=0, sum_esq=0, sum_frente=0;
-	printf("Sensor\n");
-	//para garantir que funcionao
-	disableObstSens();
-	//disableLineSens();
-	//enableLineSens();
-	enableObstSens();
-		
-	readAnalogSensors();
-	
-
-	/*int i=0;
-	for( i=0; i<9; i++){
-		a_s_dir[i]=a_s_dir[i+1];
-		a_s_esq[i]=a_s_esq[i+1];
-		a_s_frente[i]=a_s_frente[i+1];
-	}
- 
-	
-	a_s_dir[9] = obstacleSensor(OBST_SENSOR_RIGHT);
-	a_s_esq[9] = obstacleSensor(OBST_SENSOR_LEFT);
-	a_s_frente[9]= obstacleSensor(OBST_SENSOR_FRONT);
-	
-
-	*/
-	Chegada_Farol(); 		//verificao da chegada ao farol substitui => linha = lineSensor(LINE_SENSOR_CENTER);
-
-
-#if 0
-	if((ciclos%10 )== 0){			//verificar isto pois nao sei se ele ve o farol so de 5 em 5 s
-		Ver_Farol();	
-	}
-#else
-	Ver_Farol();
-#endif
-
-	
-	/*// media dos sensores
-
-	for( i=0; i<9; i++){
-		sum_dir=+ a_s_dir[i];
-		sum_esq=+ a_s_esq[i];
-		sum_frente=+a_s_frente[i];
-	}
-
-
-
-	sensor_esq = sum_dir/10;
-	sensor_dir = sum_esq/10;
-	sensor_frente= sum_dir/10;*/
-	sensor_dir = obstacleSensor(OBST_SENSOR_RIGHT);
-	sensor_esq = obstacleSensor(OBST_SENSOR_LEFT);
-	sensor_frente= obstacleSensor(OBST_SENSOR_FRONT);
-
-
-	//printf("D: %d   F: %d   E: %d     LINHA: %d    Farol: %d \r", obstacleSensor(0), obstacleSensor(1), obstacleSensor(2), linha, farolsen );
-
-}
 
 //############################################################################################################################################################3
 void Chegada_Farol ()
@@ -351,11 +282,3 @@ void Fim(){
 	}
 }
 //######################################################################################################################################################################33
-void _int_(_TIMER_4_VECTOR) isr_t22(void)
-{
-
-	Sensor();
-	printf("puta Cheguei\n");
-	
-	IFS0bits.T4IF = 0;
-}
