01f2de56bb2b0a06bed1ed26bf806c9fca46f4b9
Paulo Pereira <patp@ua.pt>
2015-03-31 09:27:05 +0100

    Drunk_Mouse2 -- The Beginnig

--- Drunk_mouse2/Drunk_Mouse2.c
+++ Drunk_mouse2/Drunk_Mouse2.c
@@ -1,35 +1,72 @@
 #include "mr32.h"
 
 void StopRobot (void);
+void mapTerrain(int, int);
 
-int estado;
+int estado = 0;
+int position [100][100]; // Tentar meter dinamico
+int index = 0;
 
 int main(void)
 {
-	
-	double x, y, t;
-
 	initPIC32();
 	closedLoopControl(true);
 	StopRobot();
 
-	setVel2(100, 100);
-	wait(10);
+	printf("Battery: %d \n", batteryVoltage());
+
+	while(1)
+	{
+		StopRobot();
+
+		if (batteryVoltage() <= 94)
+		{
+			printf("Mudar Bateria CARALHO!!");
+			StopRobot();
+		}
+
+		if (startButton() == 1)
+		{
+			estado = 1;
+		}
+
+		while(estado == 1)
+		{
+			if (stopButton() == 1)
+			{
+				estado = 0;
+				StopRobot();
+				printf("STOP BUTTON");
+			}
+			else
+			{	
+				double x, y, t;
+
+				setVel2(100, 100);
+				wait(10);
 
 
-	getRobotPos(&x, &y, &t);
-	printf("x:%f  y:%f  TETA:%f\n", x, y, t);
+				getRobotPos(&x, &y, &t);
+				printf("x:%f  y:%f  TETA:%f\n", x, y, t);
+				mapTerrain(x, y, index++);
+			}
+		}
+
+	}
 	
-
-
 }
 
-void StopRobot ()
+void StopRobot (void)
 {
 	setVel2(0, 0);
-	tick40ms = 0;
-	while(tick40ms == 0);
-	tick40ms = 0;
-	while(tick40ms == 0);
+	//tick40ms = 0;
+	//while(tick40ms == 0);
+	//tick40ms = 0;
+	//while(tick40ms == 0);
 	estado = 0;
+}
+void mapTerrain(int x, int y, int index)
+{
+	position[index][index] = 
+
 }
---
+++ Drunk_mouse2/teste.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include <math.h>
+
+int main ()
+{
+   fmod ( 3, 2 );
+}
